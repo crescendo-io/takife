0497864bc1b54af37000e6b50742160cae48039d
Bryan VIDAL <b.vidal@lonsdale.fr>
2024-05-24 18:08:01 +0200

    dev frontpage

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ add_action( 'wp_enqueue_scripts', 'wpm_enqueue_styles' );
 function wpm_enqueue_styles(){
     //wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/styles/theme.css' );
     wp_enqueue_style('theme', get_stylesheet_directory_uri() . '/styles/theme.css', array(), filemtime(get_template_directory() . '/styles/theme.css'));
+    wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap', false );
 }
 
 
--- template-parts/general/fonts.php
+++ template-parts/general/fonts.php
@@ -1,9 +1,29 @@
 <style type="text/css">
+
     @font-face {
-        font-family: 'mabry_proregular';
-        src: url('<?= get_stylesheet_directory_uri();?>/fonts/mabrypro-regular-webfont.woff2') format('woff2'),
-        url('<?= get_stylesheet_directory_uri();?>/fonts/mabrypro-regular-webfont.woff') format('woff');
-        font-weight: normal;
+        font-family: 'mattoneblack';
+        src: url('<?= get_stylesheet_directory_uri();?>/fonts/mattone-black-webfont.woff2') format('woff2'),
+        url('<?= get_stylesheet_directory_uri();?>/fonts/mattone-black-webfont.woff') format('woff');
+        font-weight: 800;
+        font-style: normal;
+
+    }
+
+
+    @font-face {
+        font-family: 'mattoneregular';
+        src: url('<?= get_stylesheet_directory_uri();?>/fonts/mattone-regular-webfont.woff2') format('woff2'),
+        url('<?= get_stylesheet_directory_uri();?>/fonts/mattone-regular-webfont.woff') format('woff');
+        font-weight: 400;
+        font-style: normal;
+
+    }
+
+    @font-face {
+        font-family: 'mattonebold';
+        src: url('<?= get_stylesheet_directory_uri();?>/fonts/mattone-bold-webfont.woff2') format('woff2'),
+        url('<?= get_stylesheet_directory_uri();?>/fonts/mattone-bold-webfont.woff') format('woff');
+        font-weight: 500;
         font-style: normal;
 
     }
