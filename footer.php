425c7e6732e42f3d7a0b540b3a238d17ce46d193
Bryan VIDAL <b.vidal@lonsdale.fr>
2025-05-14 16:19:52 +0200

    add press footer

---
+++ footer.php
@@ -0,0 +1,72 @@
+<?php
+    $option_logo_footer = get_field('option_logo_footer', 'option');
+    $option_logo_footer_array = get_custom_thumb($option_logo_footer, 'full');
+?>
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <a href="<?= get_site_url(); ?>">
+                            <img src="<?= $option_logo_footer_array['url']; ?>" class="logo-footer" alt="<?= $option_logo_footer_array['alt']; ?>">
+                        </a>
+                    </div>
+                    <div class="col-sm-3">
+                        <p>Accès Rapide</p>
+                        <?= wp_nav_menu(array(
+                            'menu'				=> "menu", // (int|string|WP_Term) Desired menu. Accepts a menu ID, slug, name, or object.
+                            'menu_class'		=> "",
+                            'container_class'	=> "menu",// (string) CSS class to use for the ul element which forms the menu. Default 'menu'.
+                        )); ?>
+                    </div>
+                    <div class="col-sm-3">
+                        <p>Liens utiles</p>
+                        <?= wp_nav_menu(array(
+                            'menu'				=> "footer", // (int|string|WP_Term) Desired menu. Accepts a menu ID, slug, name, or object.
+                            'menu_class'		=> "",
+                            'container_class'	=> "menu",// (string) CSS class to use for the ul element which forms the menu. Default 'menu'.
+                        )); ?>
+                    </div>
+                    <div class="col-sm-3">
+                        <p class="press-footer">
+                            <strong>Contact presse</strong><br/>
+                            Sarah Gerlicher Communication / Smart PR<br/>
+                            <a href="mailto:sarahgerlicher@gmail.com">sarahgerlicher@gmail.com</a><br/>
+                            06.78.29.26.25
+                        </p>
+                    </div>
+
+                    <div class="col-sm-12 text-center">
+                        <?php get_template_part('template-parts/general/bloc-social');?>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12 center">
+                        <div class="copyright">
+                            Site Web By <a href="https://crescendo-studio.io/" rel="noopener" target="_blank">Crescendo</a> ©<?= date("Y"); ?> Tous droits reservés
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+
+        <?php
+        $cookieConsent = $_COOKIE["cookieyes-consent"];
+
+        $searchConsent = strpos($cookieConsent, 'analytics:yes');
+
+        if($searchConsent && get_field('option_ga_code', 'option')):
+            echo get_field('option_ga_code', 'option');
+        endif;
+        ?>
+
+
+
+
+        <?php if(get_field('option_structured_datas', 'option')): ?>
+            <?= get_field('option_structured_datas', 'option'); ?>
+        <?php endif; ?>
+
+        <?php wp_footer(); ?>
+    </body>
+</html>
